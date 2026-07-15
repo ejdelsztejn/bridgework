d66058dbe24494742cc9ff9e91487e9de51dd3b0
Jessye Ejdelman <43380627+ejdelsztejn@users.noreply.github.com>
2026-07-15 11:53:58 -0400

    Add test formats a valid JSON payload

--- test/bridgework_web/controllers/inspector_live_test.exs
+++ test/bridgework_web/controllers/inspector_live_test.exs
@@ -10,4 +10,16 @@ defmodule BridgeworkWeb.InspectorLiveTest do
     assert has_element?(view, "#raw-payload")
     assert has_element?(view, "#payload-form")
   end
+
+  test "formats a valid JSON payload", %{conn: conn} do
+    {:ok, view, _html} = live(conn, "/")
+
+    payload = ~s({"full_name":"Melody Sampleton","email_address":"melody.sampleton@example.com"})
+
+    view
+    |> form("#payload-form", %{"payload" => payload})
+    |> render_submit()
+
+    assert has_element?(view, "#parsed-previw", "Jessy Eej Delman")
+  end
 end
